45aae31b2fbe8dffd24a9e434710c8cafe546b91
Edward Cai <edward@duolingo.com>
2018-01-11 14:56:45 -0500

    Broadcast messages

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ def index():
 
 @socketio.on('chat_send')
 def chat_send(message):
-    emit('chat_respond', message)
+    emit('chat_respond', message, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(application, host='0.0.0.0')
